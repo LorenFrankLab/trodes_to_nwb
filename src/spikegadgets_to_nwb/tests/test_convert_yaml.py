aaf5a71566f127836ec0f78c6bfc984d877a00d1
Sam Bray <sam.bray@ucsf.edu>
2023-06-26 14:57:19 -0700

    Debug pytest directory tree

--- src/spikegadgets_to_nwb/tests/test_convert_yaml.py
+++ src/spikegadgets_to_nwb/tests/test_convert_yaml.py
@@ -4,9 +4,22 @@ from pynwb.file import Subject, ProcessingModule
 from ndx_franklab_novela import Probe, Shank, ShanksElectrode
 from hdmf.common.table import DynamicTable, VectorData
 
+import os
+
+
+def test_dir():
+    print("curdir: ", os.curdir)
+    print("contents: ", os.listdir())
+    for root, dirs, files in os.walk("."):
+        path = root.split(os.sep)
+        print((len(path) - 1) * "---", os.path.basename(root))
+        for file in files:
+            print(len(path) * "---", file)
+    assert False
+
 
 def test_initial_nwb_creation():
-    metadata_path = "/tests/test_data/test_metadata.yml"
+    metadata_path = "tests/test_data/test_metadata.yml"
     metadata, _ = convert_yaml.load_metadata(metadata_path, [])
     nwb_file = convert_yaml.initialize_nwb(metadata)
     # check that things were added in
@@ -26,144 +39,144 @@ def test_initial_nwb_creation():
     assert len(nwb_file.devices) == 0
 
 
-def test_subject_creation():
-    metadata_path = "./tests/test_data/test_metadata.yml"
-    metadata, _ = convert_yaml.load_metadata(metadata_path, [])
-    nwb_file = convert_yaml.initialize_nwb(metadata)
-    convert_yaml.add_subject(nwb_file, metadata)
-    subject = nwb_file.subject
-    assert isinstance(nwb_file.subject, Subject)
-    assert subject.weight == "100 g"
-    assert isinstance(subject.sex, str) and len(subject.sex)
-    assert subject.description == "Long-Evans Rat"
-    assert subject.species == "Rattus pyctoris"
-    assert subject.genotype == "Obese Prone CD Rat"
-    assert subject.subject_id == "54321"
-    assert isinstance(subject.date_of_birth, datetime)
+# def test_subject_creation():
+#     metadata_path = "./tests/test_data/test_metadata.yml"
+#     metadata, _ = convert_yaml.load_metadata(metadata_path, [])
+#     nwb_file = convert_yaml.initialize_nwb(metadata)
+#     convert_yaml.add_subject(nwb_file, metadata)
+#     subject = nwb_file.subject
+#     assert isinstance(nwb_file.subject, Subject)
+#     assert subject.weight == "100 g"
+#     assert isinstance(subject.sex, str) and len(subject.sex)
+#     assert subject.description == "Long-Evans Rat"
+#     assert subject.species == "Rattus pyctoris"
+#     assert subject.genotype == "Obese Prone CD Rat"
+#     assert subject.subject_id == "54321"
+#     assert isinstance(subject.date_of_birth, datetime)
 
 
-def test_camera_creation():
-    metadata_path = "/src/spikegadgets/test_data/test_metadata.yml"
-    metadata, _ = convert_yaml.load_metadata(metadata_path, [])
-    nwb_file = convert_yaml.initialize_nwb(metadata)
-    convert_yaml.add_cameras(nwb_file, metadata)
-    cameras = nwb_file.devices
-    assert len(cameras) == 2
-    assert cameras["test camera 1"].meters_per_pixel == 0.001
-    assert cameras["test camera 1"].model == "model1"
-    assert cameras["test camera 1"].lens == "lens1"
-    assert cameras["test camera 1"].manufacturer == "Allied Vision"
+# def test_camera_creation():
+#     metadata_path = "/src/spikegadgets/test_data/test_metadata.yml"
+#     metadata, _ = convert_yaml.load_metadata(metadata_path, [])
+#     nwb_file = convert_yaml.initialize_nwb(metadata)
+#     convert_yaml.add_cameras(nwb_file, metadata)
+#     cameras = nwb_file.devices
+#     assert len(cameras) == 2
+#     assert cameras["test camera 1"].meters_per_pixel == 0.001
+#     assert cameras["test camera 1"].model == "model1"
+#     assert cameras["test camera 1"].lens == "lens1"
+#     assert cameras["test camera 1"].manufacturer == "Allied Vision"
 
 
-def test_acq_device_creation():
-    metadata_path = "./src/spikegadgets/tests/test_data/test_metadata.yml"
-    metadata, _ = convert_yaml.load_metadata(metadata_path, [])
-    nwb_file = convert_yaml.initialize_nwb(metadata)
-    convert_yaml.add_acquisition_devices(nwb_file, metadata)
-    devices = nwb_file.devices
-    assert len(devices) == 1
-    name = f"dataacq_deviceSpikeGadgets"
-    assert devices[name].system == "Main Control Unit"
-    assert devices[name].amplifier == "Intan"
-    assert devices[name].adc_circuit == "Intan"
+# def test_acq_device_creation():
+#     metadata_path = "./src/spikegadgets/tests/test_data/test_metadata.yml"
+#     metadata, _ = convert_yaml.load_metadata(metadata_path, [])
+#     nwb_file = convert_yaml.initialize_nwb(metadata)
+#     convert_yaml.add_acquisition_devices(nwb_file, metadata)
+#     devices = nwb_file.devices
+#     assert len(devices) == 1
+#     name = f"dataacq_deviceSpikeGadgets"
+#     assert devices[name].system == "Main Control Unit"
+#     assert devices[name].amplifier == "Intan"
+#     assert devices[name].adc_circuit == "Intan"
 
 
-def test_electrode_creation():
-    metadata_path = "/test_data/test_metadata.yml"
-    probe_metadata = [
-        "tests/test_data/tetrode_12.5.yml",
-    ]
-    metadata, probe_metadata = convert_yaml.load_metadata(metadata_path, probe_metadata)
-    nwbfile = convert_yaml.initialize_nwb(metadata)
+# def test_electrode_creation():
+#     metadata_path = "/test_data/test_metadata.yml"
+#     probe_metadata = [
+#         "tests/test_data/tetrode_12.5.yml",
+#     ]
+#     metadata, probe_metadata = convert_yaml.load_metadata(metadata_path, probe_metadata)
+#     nwbfile = convert_yaml.initialize_nwb(metadata)
 
-    # Call the function to be tested
-    convert_yaml.add_electrodeGroups(nwbfile, metadata, probe_metadata)
+#     # Call the function to be tested
+#     convert_yaml.add_electrodeGroups(nwbfile, metadata, probe_metadata)
 
-    # Perform assertions to check the results
-    # Check if the electrode groups were added correctly
-    assert len(nwbfile.electrode_groups) == len(metadata["electrode_groups"])
-    for i, group_metadata in enumerate(metadata["electrode_groups"]):
-        group = nwbfile.electrode_groups[str(group_metadata["id"])]
-        assert group.description == group_metadata["description"]
-        assert group.location == group_metadata["targeted_location"]
+#     # Perform assertions to check the results
+#     # Check if the electrode groups were added correctly
+#     assert len(nwbfile.electrode_groups) == len(metadata["electrode_groups"])
+#     for i, group_metadata in enumerate(metadata["electrode_groups"]):
+#         group = nwbfile.electrode_groups[str(group_metadata["id"])]
+#         assert group.description == group_metadata["description"]
+#         assert group.location == group_metadata["targeted_location"]
 
-    # Check if the probes were added correctly
-    assert len(nwbfile.devices) == len(metadata["electrode_groups"])
-    probe = nwbfile.devices["probe 0"]
-    probe_metadata = probe_metadata[0]
+#     # Check if the probes were added correctly
+#     assert len(nwbfile.devices) == len(metadata["electrode_groups"])
+#     probe = nwbfile.devices["probe 0"]
+#     probe_metadata = probe_metadata[0]
 
-    assert isinstance(probe, Probe)
-    assert probe.probe_type == probe_metadata["probe_type"]
-    assert probe.units == probe_metadata["units"]
-    assert probe.probe_description == probe_metadata["probe_description"]
-    assert probe.contact_side_numbering == probe_metadata["contact_side_numbering"]
-    assert probe.contact_size == probe_metadata["contact_size"]
+#     assert isinstance(probe, Probe)
+#     assert probe.probe_type == probe_metadata["probe_type"]
+#     assert probe.units == probe_metadata["units"]
+#     assert probe.probe_description == probe_metadata["probe_description"]
+#     assert probe.contact_side_numbering == probe_metadata["contact_side_numbering"]
+#     assert probe.contact_size == probe_metadata["contact_size"]
 
-    # Check if the shanks and electrodes were added correctly
-    shank_meta_list = probe_metadata["shanks"]
-    assert len(probe.shanks) == len(shank_meta_list)
-    for j, shank_meta in enumerate(shank_meta_list):
-        shank = probe.shanks[str(j)]
-        assert isinstance(shank, Shank)
-        assert len(shank.shanks_electrodes) == len(shank_meta["electrodes"])
-        for k, electrode_meta in enumerate(shank_meta["electrodes"]):
-            electrode = shank.shanks_electrodes[str(k)]
-            assert isinstance(electrode, ShanksElectrode)
-            assert electrode.rel_x == float(electrode_meta["rel_x"])
-            assert electrode.rel_y == float(electrode_meta["rel_y"])
-            assert electrode.rel_z == float(electrode_meta["rel_z"])
+#     # Check if the shanks and electrodes were added correctly
+#     shank_meta_list = probe_metadata["shanks"]
+#     assert len(probe.shanks) == len(shank_meta_list)
+#     for j, shank_meta in enumerate(shank_meta_list):
+#         shank = probe.shanks[str(j)]
+#         assert isinstance(shank, Shank)
+#         assert len(shank.shanks_electrodes) == len(shank_meta["electrodes"])
+#         for k, electrode_meta in enumerate(shank_meta["electrodes"]):
+#             electrode = shank.shanks_electrodes[str(k)]
+#             assert isinstance(electrode, ShanksElectrode)
+#             assert electrode.rel_x == float(electrode_meta["rel_x"])
+#             assert electrode.rel_y == float(electrode_meta["rel_y"])
+#             assert electrode.rel_z == float(electrode_meta["rel_z"])
 
-    # Check if the electrode table was extended correctly
-    assert len(nwbfile.electrodes.columns) == 13
+#     # Check if the electrode table was extended correctly
+#     assert len(nwbfile.electrodes.columns) == 13
 
 
-def test_add_tasks():
-    # Set up test data
-    metadata_path = "./test_data/test_metadata.yml"
-    metadata, _ = convert_yaml.load_metadata(metadata_path, [])
-    nwbfile = convert_yaml.initialize_nwb(metadata)
+# def test_add_tasks():
+#     # Set up test data
+#     metadata_path = "./test_data/test_metadata.yml"
+#     metadata, _ = convert_yaml.load_metadata(metadata_path, [])
+#     nwbfile = convert_yaml.initialize_nwb(metadata)
 
-    # Call the function to be tested
-    convert_yaml.add_tasks(nwbfile, metadata)
+#     # Call the function to be tested
+#     convert_yaml.add_tasks(nwbfile, metadata)
 
-    # Perform assertions to check the results
-    # Check if the processing module was added correctly
-    assert "tasks" in nwbfile.processing
-    tasks_module = nwbfile.processing["tasks"]
-    assert isinstance(tasks_module, ProcessingModule)
-    assert tasks_module.name == "tasks"
-    assert tasks_module.description == "Contains all tasks information"
+#     # Perform assertions to check the results
+#     # Check if the processing module was added correctly
+#     assert "tasks" in nwbfile.processing
+#     tasks_module = nwbfile.processing["tasks"]
+#     assert isinstance(tasks_module, ProcessingModule)
+#     assert tasks_module.name == "tasks"
+#     assert tasks_module.description == "Contains all tasks information"
 
-    # Check if the tasks were added correctly
-    assert len(tasks_module.data_interfaces) == len(metadata["tasks"])
-    for i, task_metadata in enumerate(metadata["tasks"]):
-        task = tasks_module.data_interfaces[f"task_{i}"]
-        assert isinstance(task, DynamicTable)
-        assert task.name == f"task_{i}"
-        assert task.description == ""
-        assert len(task.columns) == 5
+#     # Check if the tasks were added correctly
+#     assert len(tasks_module.data_interfaces) == len(metadata["tasks"])
+#     for i, task_metadata in enumerate(metadata["tasks"]):
+#         task = tasks_module.data_interfaces[f"task_{i}"]
+#         assert isinstance(task, DynamicTable)
+#         assert task.name == f"task_{i}"
+#         assert task.description == ""
+#         assert len(task.columns) == 5
 
-        # Check if the task metadata columns were added correctly
-        for val in task.columns:
-            assert isinstance(val, VectorData)
-        for a, b in zip(
-            task.colnames,
-            (
-                "task_name",
-                "task_description",
-                "camera_id",
-                "task_epochs",
-                "task_environment",
-            ),
-        ):
-            assert a == b
+#         # Check if the task metadata columns were added correctly
+#         for val in task.columns:
+#             assert isinstance(val, VectorData)
+#         for a, b in zip(
+#             task.colnames,
+#             (
+#                 "task_name",
+#                 "task_description",
+#                 "camera_id",
+#                 "task_epochs",
+#                 "task_environment",
+#             ),
+#         ):
+#             assert a == b
 
-        # Check if the task metadata values were added correctly
-        task_df = task.to_dataframe()
-        assert task_df["task_name"][0] == task_metadata["task_name"]
-        assert task_df["task_description"][0] == task_metadata["task_description"]
-        assert task_df["camera_id"][0] == [int(id) for id in task_metadata["camera_id"]]
-        assert task_df["task_epochs"][0] == [
-            int(epoch) for epoch in task_metadata["task_epochs"]
-        ]
-        assert task_df["task_environment"][0] == task_metadata["task_environment"]
+#         # Check if the task metadata values were added correctly
+#         task_df = task.to_dataframe()
+#         assert task_df["task_name"][0] == task_metadata["task_name"]
+#         assert task_df["task_description"][0] == task_metadata["task_description"]
+#         assert task_df["camera_id"][0] == [int(id) for id in task_metadata["camera_id"]]
+#         assert task_df["task_epochs"][0] == [
+#             int(epoch) for epoch in task_metadata["task_epochs"]
+#         ]
+#         assert task_df["task_environment"][0] == task_metadata["task_environment"]
