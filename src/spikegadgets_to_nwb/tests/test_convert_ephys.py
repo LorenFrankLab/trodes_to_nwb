ecfa013d3414474e530b92a8dd87c1402ea622b4
Sam Bray <sam.bray@ucsf.edu>
2023-07-20 10:19:04 -0700

    Add test for two epoch ephys session

--- src/spikegadgets_to_nwb/tests/test_convert_ephys.py
+++ src/spikegadgets_to_nwb/tests/test_convert_ephys.py
@@ -182,3 +182,98 @@ def test_add_raw_ephys_single_rec_probe_configuration():
             ).all()
 
     # TODO remove the file
+
+
+def test_add_raw_ephys_two_epoch():
+    # load metadata yml and make nwb file
+    metadata_path = path + "/test_data/test_metadata.yml"
+    probe_metadata = [
+        path + "/test_data/tetrode_12.5.yml",
+    ]
+    metadata, probe_metadata = convert_yaml.load_metadata(metadata_path, probe_metadata)
+    nwbfile = convert_yaml.initialize_nwb(metadata)
+
+    # create the hw_channel map using the reconfig header
+    try:
+        # running on github
+        trodesconf_file = os.environ.get("DOWNLOAD_DIR") + "/20230622_155936.rec"
+        rec_header = convert_rec_header.read_header(trodesconf_file)
+    except:
+        # running locally
+        trodesconf_file = path + "/test_data/20230622_155936.rec"
+        rec_header = convert_rec_header.read_header(trodesconf_file)
+
+    hw_channel_map = convert_rec_header.make_hw_channel_map(
+        metadata, rec_header.find("SpikeConfiguration")
+    )
+    ref_electrode_map = convert_rec_header.make_ref_electrode_map(
+        metadata, rec_header.find("SpikeConfiguration")
+    )
+
+    convert_yaml.add_electrode_groups(
+        nwbfile, metadata, probe_metadata, hw_channel_map, ref_electrode_map
+    )
+
+    try:
+        # running on github
+        recfile = [
+            os.environ.get("DOWNLOAD_DIR") + "/20230622_155936.rec",
+            os.environ.get("DOWNLOAD_DIR") + "/20230622_160016.rec",
+        ]
+        rec_to_nwb_file = os.environ.get("DOWNLOAD_DIR") + "/minirec20230622_.nwb"
+    except (TypeError, FileNotFoundError):
+        # running locally
+        recfile = [
+            path + "/test_data/20230622_155936.rec",
+            path + "/test_data/20230622_160016.rec",
+        ]
+        rec_to_nwb_file = path + "/test_data/minirec20230622_.nwb"
+    map_row_ephys_data_to_row_electrodes_table = list(range(len(nwbfile.electrodes)))
+
+    add_raw_ephys(
+        nwbfile,
+        recfile,
+        map_row_ephys_data_to_row_electrodes_table,
+        conversion=float(metadata["raw_data_to_volts"]),
+    )
+
+    filename = "test_add_raw_ephys_single_rec.nwb"
+    with pynwb.NWBHDF5IO(filename, "w") as io:
+        io.write(nwbfile)
+
+    with pynwb.NWBHDF5IO(filename, "r", load_namespaces=True) as io:
+        read_nwbfile = io.read()
+        assert "e-series" in read_nwbfile.acquisition
+        assert read_nwbfile.acquisition["e-series"].data.chunks == (16384, 32)
+
+        with pynwb.NWBHDF5IO(rec_to_nwb_file, "r", load_namespaces=True) as io2:
+            old_nwbfile = io2.read()
+            print(
+                read_nwbfile.acquisition["e-series"].data.shape,
+                old_nwbfile.acquisition["e-series"].data.shape,
+            )
+
+            # check ordering worked correctly
+            conversion = (
+                read_nwbfile.acquisition["e-series"].conversion * MICROVOLTS_PER_VOLT
+            )
+            assert (
+                (read_nwbfile.acquisition["e-series"].data[0, :] * conversion).astype(
+                    "int16"
+                )
+                == old_nwbfile.acquisition["e-series"].data[0, :]
+            ).all()
+            # check data shapes match
+            assert (
+                read_nwbfile.acquisition["e-series"].data.shape
+                == old_nwbfile.acquisition["e-series"].data.shape
+            )
+            # check all values of one of the streams
+            assert (
+                (read_nwbfile.acquisition["e-series"].data[:, 0] * conversion).astype(
+                    "int16"
+                )
+                == old_nwbfile.acquisition["e-series"].data[:, 0]
+            ).all()
+
+    # TODO remove the file
