0934474bf3f42b426dd83f866029049e84fb44ed
Sam Bray <sam.bray@ucsf.edu>
2023-08-08 14:33:15 -0700

    Fix test to expect flat data structure

--- src/spikegadgets_to_nwb/tests/test_convert.py
+++ src/spikegadgets_to_nwb/tests/test_convert.py
@@ -15,19 +15,21 @@ def test_get_file_info():
         data_path = Path(path)
     path_df = get_file_info(data_path)
 
-    assert len(path_df[(path_df.epoch == 1) & (path_df.file_extension == ".rec")]) == 2
-    assert len(path_df[(path_df.epoch == 2) & (path_df.file_extension == ".rec")]) == 2
     for file_type in [
         ".h264",
         ".stateScriptLog",
         ".cameraHWSync",
         ".videoTimeStamps",
         ".videoPositionTracking",
+        ".rec",
     ]:
         assert len(path_df[path_df.file_extension == file_type]) == 2
 
     assert set(path_df.animal) == {"sample"}
     assert set(path_df.date) == {20230622}
+    assert set(path_df.epoch) == {1, 2}
     assert (set(path_df.tag) == {"a1"}) or (
         set(path_df.tag) == {"a1", "NA"}
     )  # yamlfiles only added in local testing
+    for file in path_df.full_path:
+        assert Path(file).exists()
