d2217030eab34c03b17faddfeb2fff99a749f275
Sam Bray <sam.bray@ucsf.edu>
2023-08-20 11:06:14 -0700

    Test epoch interval stop times.

--- src/spikegadgets_to_nwb/tests/test_convert_intervals.py
+++ src/spikegadgets_to_nwb/tests/test_convert_intervals.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 import os
+from pynwb import NWBHDF5IO
 
 from spikegadgets_to_nwb.data_scanner import get_file_info
 from spikegadgets_to_nwb.convert_intervals import add_epochs
 from spikegadgets_to_nwb.convert_yaml import initialize_nwb, load_metadata
 from spikegadgets_to_nwb.tests.test_convert_rec_header import default_test_xml_tree
+from spikegadgets_to_nwb.spike_gadgets_raw_io import SpikeGadgetsRawIO
 
 path = os.path.dirname(os.path.abspath(__file__))
 
@@ -16,14 +18,29 @@ def test_add_epochs():
     try:
         # running on github
         file_info = get_file_info(Path(os.environ.get("DOWNLOAD_DIR")))
+        rec_to_nwb_file = (
+            os.environ.get("DOWNLOAD_DIR") + "/probe_reconfig_20230622_155936.nwb"
+        )
+        rec_to_nwb_file = os.environ.get("DOWNLOAD_DIR") + "/minirec20230622_.nwb"
     except (TypeError, FileNotFoundError):
         # running locally
         file_info = get_file_info(Path(path))
-    add_epochs(nwbfile, file_info, 20230622, "sample")
+        rec_to_nwb_file = path + "/test_data/minirec20230622_.nwb"
+    # get all streams for all files
+    neo_io = [
+        SpikeGadgetsRawIO(filename=file)
+        for file in file_info[file_info.file_extension == ".rec"].full_path
+    ]
+    [neo_io.parse_header() for neo_io in neo_io]
+    add_epochs(nwbfile, file_info, 20230622, "sample", neo_io)
     epochs_df = nwbfile.epochs.to_dataframe()
+    # load old nwb versio
+    io = NWBHDF5IO(rec_to_nwb_file, "r")
+    old_nwbfile = io.read()
+    old_epochs_df = old_nwbfile.epochs.to_dataframe()
 
     assert len(epochs_df) == 2
     assert list(epochs_df.index) == [0, 1]
     assert list(epochs_df.tags) == [["01_a1"], ["02_a1"]]
     assert list(epochs_df.start_time) == [1687474797.888, 1687474821.109]
-    assert list(epochs_df.stop_time) == [0.0, 0.0]
+    assert list(epochs_df.stop_time) == list(old_epochs_df.stop_time)
