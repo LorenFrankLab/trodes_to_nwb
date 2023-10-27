15a310d1874d2acf34be9afcf265cb9b3ca62264
rly <rly@lbl.gov>
2023-10-27 14:01:12 -0700

    Test

--- src/spikegadgets_to_nwb/tests/utils.py
+++ src/spikegadgets_to_nwb/tests/utils.py
@@ -10,7 +10,12 @@ data_path = os.environ.get("DOWNLOAD_DIR", None)
 if data_path is not None:
     # running from the GitHub Action workflow
     data_path = Path(data_path)
-    shutil.copytree(yaml_path, data_path, dirs_exist_ok=True)
+    shutil.copytree(
+        yaml_path,
+        data_path,
+        dirs_exist_ok=True,
+        ignore=shutil.ignore_patterns(*os.listdir(data_path)),  # ignore existing
+    )
 else:
     # running locally -- bulk test data is the same directory as the test yaml files
     data_path = yaml_path
