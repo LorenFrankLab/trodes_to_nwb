1c5e50dd717ba8587789ed8caccbcba9fbb52f17
Sam Bray <sam.bray@ucsf.edu>
2023-11-22 14:34:20 -0800

    add test for git_data multiplex channel slicing

--- src/trodes_to_nwb/tests/test_convert_analog.py
+++ src/trodes_to_nwb/tests/test_convert_analog.py
@@ -1,11 +1,12 @@
 import os
 
 import pynwb
-from trodes_to_nwb.convert_analog import add_analog_data, get_analog_channel_names
-from trodes_to_nwb.tests.test_convert_rec_header import default_test_xml_tree
-from trodes_to_nwb.tests.utils import data_path
 
 from trodes_to_nwb import convert_rec_header, convert_yaml
+from trodes_to_nwb.convert_analog import add_analog_data, get_analog_channel_names
+from trodes_to_nwb.convert_ephys import RecFileDataChunkIterator
+from trodes_to_nwb.tests.test_convert_rec_header import default_test_xml_tree
+from trodes_to_nwb.tests.utils import data_path
 
 
 def test_add_analog_data():
@@ -31,7 +32,7 @@ def test_add_analog_data():
         assert "analog" in read_nwbfile.processing["analog"]["analog"].time_series
         assert read_nwbfile.processing["analog"]["analog"]["analog"].data.chunks == (
             16384,
-            12,
+            22,
         )
 
         with pynwb.NWBHDF5IO(rec_to_nwb_file, "r", load_namespaces=True) as io2:
@@ -68,3 +69,37 @@ def test_add_analog_data():
             ).all()
     # cleanup
     os.remove(filename)
+
+
+def test_selection_of_multiplexed_data():
+    rec_file = data_path / "20230622_sample_01_a1.rec"
+    rec_header = convert_rec_header.read_header(rec_file)
+    hconf = rec_header.find("HardwareConfiguration")
+    ecu_conf = None
+    for conf in hconf:
+        if conf.attrib["name"] == "ECU":
+            ecu_conf = conf
+            break
+    analog_channel_ids = []
+    for channel in ecu_conf:
+        if channel.attrib["dataType"] == "analog":
+            analog_channel_ids.append(channel.attrib["id"])
+    assert (len(analog_channel_ids)) == 12
+    rec_dci = RecFileDataChunkIterator(
+        [rec_file],
+        nwb_hw_channel_order=analog_channel_ids,
+        stream_index=2,
+        is_analog=True,
+    )
+    assert len(rec_dci.neo_io[0].multiplexed_channel_xml.keys()) == 10
+    slice_ind = [(0, 4), (0, 30), (1, 15), (5, 15), (20, 25)]
+    expected_channels = [4, 22, 14, 10, 2]
+    for ind, expected in zip(slice_ind, expected_channels):
+        data = rec_dci._get_data(
+            (
+                slice(0, 100, None),
+                slice(ind[0], ind[1], None),
+            )
+        )
+        assert data.shape[1] == expected
+    return
